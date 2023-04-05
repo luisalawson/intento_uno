ba62207de90018f12913cc9d253e51660ffbc2cc
luisalawson <79951357+luisalawson@users.noreply.github.com>
2023-04-04 23:03:08 -0400

    Update script_4.js

--- etapauno/script_4.js
+++ etapauno/script_4.js
@@ -1,30 +1,18 @@
 let chart_4
 d3.csv('astronautas.csv', d3.autoType).then(data => {
   chart_4 = Plot.plot({
-    grid: true,
-    nice: true,
-    zero: true,
-    color: {
-      legend: true,
-    },
-    facet: {
-      data: data,
-      x: "edad_mision",
-    },
-    marginLeft: 80,
-    marginRight: 80,
-    marks: [
-      Plot.dot(data, {
-        x: "edad_mision",
-        y: "genero",
-        fill: "edad_mision",
-        fillOpacity: 0.6,
-        r: "anio_mision",
-        title: "nombre",
-      }),
-    ],
-  });
+  height: 280,
+  x: {
+    percent: true
+  },
+  facet: {
+    data: data,
+  },
+  marks: [
+    Plot.barX(data, Plot.stackX(Plot.groupZ({x: "anio_mision"}, {fill: "gender"}))),
+    Plot.ruleX([0, 1])
+  ]
+});
   d3.select('#chart_4').append(() => chart_4)
 })
-// Mi idea era mostrar cuantas mujeres y cuantos hombres habian hecho misiones y cuanto les tardo llegar a esa mision(edad)
-//Ver como hacer para que queden más lindos los nombres de los ejes
+
