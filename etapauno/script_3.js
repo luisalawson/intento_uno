f1e6949281ca717e3442851b82ffdc006e6a897d
luisalawson <79951357+luisalawson@users.noreply.github.com>
2023-04-04 22:58:42 -0400

    Update script_3.js

--- etapauno/script_3.js
+++ etapauno/script_3.js
@@ -2,25 +2,17 @@ d3.csv("astronautas.csv", d3.autoType).then((data) => {
   let chart_3 = Plot.plot({
     
     marks: [
-      Plot.dot(data, {
-        x: "anio_mision",
-        y: "nacionalidad",
-        r: "edad_mision",
-        fill: "anio_mision",
-        symbol: "anio_mision",
-      }),
-    ],
-    grid: true,
-    line: true,
-    nice: true,
-    symbol: {
-      legend: "true",
-    },
-    
-
-  });
-
+      Plot.plot({
+  x: {
+    type: "band",
+    tickFormat: d => d.toLocaleString("en", {month: "narrow"}),
+    label: null
+  },
+  marks: [
+    Plot.barY(data, {x: "anio_mision", y: "nacionalidad", fill: "nacionalidad"}),
+    Plot.ruleY([0])
+  ]
+})
   d3.select("#chart_3").append(() => chart_3);
 });
-//SE PUDOOOO
-//Hay que ver como hacer para que los paises no se choquen con el nombre del y-axis
+
